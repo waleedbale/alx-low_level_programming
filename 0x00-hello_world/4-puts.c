b3284eae3260ff8475926073fec6d4cbbfced489
Waleedkrish <waleedbale27@gmail.com>
2022-03-11 18:29:08 +0100

    My commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#Include <stdio.h>
+/**
+ * main - entry oint
+ *
+ * Return: 0 after printing the function 
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle"):
+return (0);
+{
