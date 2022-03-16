40005ddcffc4191db55b7b4a23796909b8a899cb
waleedbale <waleedbale27@gmail.com>
2022-03-16 11:29:12 +0100

    Commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#Include <stdio.h>
+#include <stdio.h>
 /**
- * main - entry oint
- *
- * Return: 0 after printing the function 
- */
+*main - entry point
+*
+*Return: 0 after printing the function
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle"):
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
-{
+}
