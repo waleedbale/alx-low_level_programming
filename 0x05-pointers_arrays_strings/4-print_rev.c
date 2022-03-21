f9541465a411f63456785203749a743501a1e1fa
waleedbale <waleedbale27@gmail.com>
2022-03-21 16:37:44 +0100

    My commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,22 +5,23 @@
  *
  * @str: in reverse.
  */
+
 void print_rev(char *str)
 {
-int i = 0;
-int total_caracteres = 0;
-int c = 0;
+	int i = 0;
+	int total_caracteres = 0;
+	int c = 0;
 
-while (str[i] != 0)
-{
-i++;
-}
-total_caracteres =i - 1;
+	while (str[i] != 0)
+	{
+		i++;
+	}
+	total_caracteres = i - 1;
 
-for (c = 0; c <= total caracteres; c++)
-{
--putchar(str[total_caracteres - c]);
-}
+	for (c = 0; c <= total_caracteres; c++)
+	{
+		_putchar(str[total_caracteres - c]);
+	}
 
--putchar('\');
+	_putchar('\n');
 }
