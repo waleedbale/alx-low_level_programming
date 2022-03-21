4878128859f300a3c1b8a489a73da6636d5822d7
waleedbale <waleedbale27@gmail.com>
2022-03-21 16:24:53 +0100

    My commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * print_rev - function that prints a string, in reverse, followed by a new line.
+ * print_rev - function that prints a string, in reverse,
+ * followed by a new line
  *
  * @str: in reverse.
  */
-void print_rev(char *s)
+void print_rev(char *str)
 {
 int i = 0;
 int total_caracteres = 0;
@@ -16,7 +17,7 @@ i++;
 }
 total_caracteres =i - 1;
 
-for (c = 0; c <= total caracteres;
+for (c = 0; c <= total caracteres; c++)
 {
 -putchar(str[total_caracteres - c]);
 }
